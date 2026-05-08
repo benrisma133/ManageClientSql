7cb06f29306eed2698ddc529bb7253e1fcccdeac
Ismail Benrahhal <ibenrahhal133@gmail.com>
2026-05-08 05:37:21 +0100

    feat: implement ClientService, tests, and auto-create database

--- ManageClientSql.Repository/Models/Client.cs
+++ ManageClientSql.Repository/Models/Client.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ManageClientSql.Repository.Models;
 
-namespace ManageClientSql.Repository.Models
+public class Client
 {
-    internal class Client
-    {
-    }
+    public int Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
---
+++ ManageClientSql.Service/ClientService.cs
@@ -0,0 +1,87 @@
+﻿using ManageClientSql.Repository;
+using ManageClientSql.Repository.Models;
+
+namespace ManageClientSql.Service;
+
+public class ClientService
+{
+    public enum enMode { AddNew, Update }
+    public enMode Mode { get; private set; } = enMode.AddNew;
+
+    public Client Client { get; set; } = new Client();
+
+    private bool _AddNew()
+    {
+        if (ClientRepository.IsClientExistByPhone(Client.PhoneNumber))
+            return false;
+
+        if (ClientRepository.IsClientExistByEmail(Client.Email))
+            return false;
+
+        int newId = ClientRepository.Add(Client);
+
+        if (newId > 0)
+        {
+            Client.Id = newId;
+            Mode = enMode.Update; // switch to Update mode after successful add
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool _Update()
+    {
+        if (ClientRepository.IsClientExistByPhone(Client.PhoneNumber, Client.Id))
+            return false;
+
+        if (ClientRepository.IsClientExistByEmail(Client.Email, Client.Id))
+            return false;
+
+        return ClientRepository.Update(Client);
+    }
+
+    public bool Save()
+    {
+        switch (Mode)
+        {
+            case enMode.AddNew:
+                return _AddNew();
+
+            case enMode.Update:
+                return _Update();
+
+            default:
+                return false;
+        }
+    }
+
+    public bool Delete()
+    {
+        return ClientRepository.Delete(Client.Id);
+    }
+
+    public List<Client> GetAll()
+    {
+        return ClientRepository.GetAll();
+    }
+
+    public Client? GetClientById(int id)
+    {
+        return ClientRepository.GetClientById(id);
+    }
+
+    // Load existing client for editing → mode becomes Update
+    public static ClientService Load(int id)
+    {
+        var client = ClientRepository.GetClientById(id);
+        if (client == null)
+            throw new Exception($"Client with ID {id} not found.");
+
+        return new ClientService
+        {
+            Client = client,
+            Mode = enMode.Update
+        };
+    }
+}
